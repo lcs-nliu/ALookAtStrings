d18683c22e9eaef2de671c3b9c21c544254bf598
Nicole Liu <nicoleliu_@outlook.com>
2019-11-20 11:30:28 -0500

    Finished last exercise

--- ALookAtStrings.playground/Contents.swift
+++ ALookAtStrings.playground/Contents.swift
@@ -185,6 +185,24 @@ Say that you wish to encrypt the word `pen` using a *rightward shift of three ch
 */
 
 // Create the starting phrase
+print ("=========================")
 let plainText = "pen"
+var cipherText = ""
+
+for scalar in plainText.unicodeScalars {
+    
+    print(scalar)
+    print (scalar.value)
+    // shift each scalar value by 3
+    let newScalarValue = scalar.value+3
+    // Attempt to convert the scalar value to an actual scalar (character)
+    if let newScalar = UnicodeScalar(newScalarValue){
+        print(newScalarValue)
+        print(newScalar)
+        cipherText += String(newScalar)
+    }
+}
+print ("The encrypted text is \(cipherText).")
+
 
 // Encrypt the starting phrase
